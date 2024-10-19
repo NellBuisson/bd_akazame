0ca95056b1ed1babccec3ea73845286b1eb91ef5
Nell Buisson <nell.buisson@outlook.fr>
2024-10-19 20:33:07 +0200

    Création du dossier pour la base de données. Table créée études, début création table personne

---
+++ bd_akazame.sql
@@ -0,0 +1,10 @@
+-- Active: 1729358910179@@127.0.0.1@3306@akazame
+CREATE TABLE IF NOT EXISTS Etudes (
+    nometude VARCHAR(20) PRIMARY KEY,
+    domaine VARCHAR(20),
+    nbrannee INT
+);
+
+CREATE TABLE IF NOT EXISTS Personnes (
+    nom 
+)
